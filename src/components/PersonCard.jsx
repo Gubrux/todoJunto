9f09aa1d60bd59b414c4dcf19af8b3d45522c267
Gubrux <gabrielcentur@gmail.com>
2024-03-06 11:37:15 -0300

    se agrega el codigo para componente funcional

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -8,18 +8,37 @@ class PersonCard extends Component{
         };
     }
     handleClick = () => {
-        this.setState({age:this.state.age + 1})
+        this.setState({age:this.state.age +1})
     }
     render(){
-        const {firstName, lastName, hairColor, className} = this.props
+        const {firstName, lastName, hairColor} = this.props
         return(
             <div className="Card">
                 <h1>{lastName}, {firstName}</h1>
                 <p>Age: {this.state.age}</p>
-                <p className={className}>Hair Color: {hairColor}</p>
+                <p>Hair Color: {hairColor}</p>
                 <button onClick={this.handleClick}>Birthday for: {firstName} {lastName}</button>
             </div>
         )
     }
 }
+// con componente funcional:
+// import React, { useState } from "react";
+// import '../App.css';
+
+// const PersonCard = ({ firstName, lastName, hairColor, age }) => {
+//     const [currentAge, setCurrentAge] = useState(age);
+
+//     const handleClick = () => {
+//         setCurrentAge(currentAge + 1);
+//     }
+//     return (
+//         <div className="Card">
+//             <h1>{lastName}, {firstName}</h1>
+//             <p>Age: {currentAge}</p>
+//             <p>Hair Color: {hairColor}</p>
+//             <button onClick={handleClick}>Birthday for: {firstName} {lastName}</button>
+//         </div>
+//     );
+// }
 export default PersonCard;
